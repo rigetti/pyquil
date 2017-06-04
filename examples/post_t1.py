0ff6aa9b49df041691f929f753f13f47e8c79f9e
Will Zeng <will@rigetti.com>
2017-06-03 21:59:21 -0700

    update t1 test script

--- examples/post_t1.py
+++ examples/post_t1.py
@@ -1,4 +1,4 @@
 import pyquil.forest as forest
 qpu = forest.QPUConnection("Z12-13-C4a2")
-res = qpu.t1(qubit_id=3, start=0.01, stop=60.0, num_pts=51)
+res = qpu.t1(qubit_id=3, start=0.01, stop=60.0, num_pts=31)
 print res.success, res.result
