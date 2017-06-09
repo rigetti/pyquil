d4c26e7058204175eff545e50df6bc55427e138e
Will Zeng <will@rigetti.com>
2017-06-09 10:35:36 -0700

    tick to 0.1.0 version

--- setup.py
+++ setup.py
@@ -19,12 +19,12 @@ from setuptools import setup
 
 setup(
     name = "pyquil",
-    version = "0.0.4",
+    version = "0.1.0",
     author = "Rigetti Computing",
     author_email = "softapps@rigetti.com",
     description = "A Python library to generate Quantum Instruction Language (Quil) Programs.",
     url="https://github.com/rigetticomputing/pyquil.git",
-    download_url="https://github.com/rigetticomputing/pyquil/tarball/0.0.4",
+    download_url="https://github.com/rigetticomputing/pyquil/tarball/0.1.0",
     packages = ["pyquil"],
     license = "LICENSE",
     install_requires = [
