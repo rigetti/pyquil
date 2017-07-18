9a75b56f75fa8693c5552886648c35cded27092b
richardzhu <richard.zhu@rigetti.com>
2017-07-17 17:53:04 -0700

    Test added for is_done()

--- pyquil/tests/test_job_results.py
+++ pyquil/tests/test_job_results.py
@@ -1,4 +1,8 @@
 from pyquil.job_results import _round_to_next_multiple, _octet_bits
+from pyquil.job_results import JobResult
+from pyquil.qpu import QPUConnection
+import mock
+from mock import patch
 
 
 def test_rounding():
@@ -17,3 +21,16 @@ def test_octet_bits():
     assert [0, 1, 0, 0, 0, 0, 0, 0] == _octet_bits(0b10)
     assert [1, 0, 1, 0, 0, 0, 0, 0] == _octet_bits(0b101)
     assert [1, 1, 1, 1, 1, 1, 1, 1] == _octet_bits(0b11111111)
+
+
+def test_get_is_called():
+    # check that JobResult.is_done() calls .get() on QPU prior to
+    # returning result
+    qpu_connect = QPUConnection('test')
+    job_result = JobResult(qpu_connect, False)
+    try:
+        with patch.object(job_result, 'get') as mock:
+            job_result.is_done()
+    except:
+        pass
+    mock.assert_called_with()
