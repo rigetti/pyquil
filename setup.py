5167e48cd4e5aa27d86e8327a4fdaa7cebbbfcea
Steven Heidel <steven@rigetti.com>
2017-11-11 15:25:56 -0800

    Fix setup issues and 1.3.1 release

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ setup(
     author_email="softapps@rigetti.com",
     description="A Python library to generate Quantum Instruction Language (Quil) Programs.",
     url="https://github.com/rigetticomputing/pyquil.git",
-    packages=['pyquil', 'pyquil._parser', 'pyquil.setup'],
+    packages=['pyquil', 'pyquil._parser', 'pyquil._parser.gen2', 'pyquil._parser.gen3', 'pyquil.setup'],
     license="LICENSE",
     install_requires=[
         'requests >= 2.4.2',
