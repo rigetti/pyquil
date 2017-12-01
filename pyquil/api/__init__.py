f737c70b3f87c61883544f68a9dda6c435cf5884
Steven Heidel <steven@heidel.ca>
2017-12-01 14:44:25 -0800

    Add get_devices and ability to specify device_name (#209)
    * Add get_devices and ability to specify device_name
    
    * Update docs

--- pyquil/api/__init__.py
+++ pyquil/api/__init__.py
@@ -18,11 +18,11 @@ Module for facilitating connections to the QVM / QPU.
 """
 import warnings
 
-__all__ = ['QVMConnection', 'QPUConnection']
+__all__ = ['QVMConnection', 'QPUConnection', 'Job', 'get_devices']
 
 from pyquil.api.job import Job
 from pyquil.api.qvm import QVMConnection
-from pyquil.api.qpu import QPUConnection
+from pyquil.api.qpu import QPUConnection, get_devices
 
 
 class SyncConnection(QVMConnection):
