05c0094467e405c3bcc36d8a14528a44bec4eed4
mstechly <michal.stechly@gmail.com>
2017-12-03 09:46:28 +0100

    Updated naming convention of pyquil.api in post_ examples.

--- examples/post_t1.py
+++ examples/post_t1.py
@@ -1,4 +1,4 @@
-import pyquil.api as forest
-qpu = forest.QPUConnection("Z12-13-C4a2")
+from pyquil import api
+qpu = api.QPUConnection("Z12-13-C4a2")
 res = qpu.t1(qubit_id=3, start=0.01, stop=60.0, num_pts=31)
 print res.success, res.result
