93bc940024dbb9259415291f5c29bfe323e94409
mstechly <michal.stechly@gmail.com>
2017-12-03 09:48:42 +0100

    Updated print functions in post_ examples.

--- examples/post_ramsey.py
+++ examples/post_ramsey.py
@@ -1,4 +1,4 @@
 from pyquil import api
 qpu = api.QPUConnection("Z12-13-C4a2")
 res = qpu.ramsey(qubit_id=3, start=0.01, stop=10, step=0.2, detuning=0.5)
-print res.success, res.result
+print(res.success, res.result)
