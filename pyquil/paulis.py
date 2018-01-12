b065363c40c4ba3f1ecc66d8a61a6d7cc109237b
c. b. osborn <chris.osborn@gmail.com>
2018-01-12 13:33:15 -0800

    Resolves #263. exponentiate_pauli_sum, and test. (#264)
    * Resolves #263. exponentiate_pauli_sum, and test.
    
    * styling
    
    * Renamed function to exponential_commuting_pauli_sum for clarity, modified docs and tests.
    
    * Program(...) instead of sum(..., Program()).

--- pyquil/paulis.py
+++ pyquil/paulis.py
@@ -696,6 +696,26 @@ def exponential_map(term):
     return exp_wrap
 
 
+def exponentiate_commuting_pauli_sum(pauli_sum):
+    """
+    Returns a function that maps all substituent PauliTerms and sums them into a program. NOTE: Use
+    this function with care. Substituent PauliTerms should commute.
+
+    :param PauliSum pauli_sum: PauliSum to exponentiate.
+    :returns: A function that parametrizes the exponential.
+    :rtype: function
+    """
+    if not isinstance(pauli_sum, PauliSum):
+        raise TypeError("Argument 'pauli_sum' must be a PauliSum.")
+
+    fns = [exponential_map(term) for term in pauli_sum]
+
+    def combined_exp_wrap(param):
+        return Program([f(param) for f in fns])
+
+    return combined_exp_wrap
+
+
 def _exponentiate_general_case(pauli_term, param):
     """
     Returns a Quil (Program()) object corresponding to the exponential of
