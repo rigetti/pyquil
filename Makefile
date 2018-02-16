rigetti/pyquil

commit e9f4531a9878770a574fff075b02eba9b2a29731

Modified ISA dictionary to index by "1Q", "2Q", and removed "id". Resolves #309.

pyquil/device.py
@@ -29,16 +29,13 @@ DEFAULT_EDGE_TYPE = "CZ"
 
 Qubit = namedtuple("Qubit", ["id", "type", "dead"])
 Edge = namedtuple("Edge", ["targets", "type", "dead"])
-_ISA = namedtuple("_ISA", ["name", "version", "timestamp", "qubits", "edges"])
+_ISA = namedtuple("_ISA", ["qubits", "edges"])
 
 
 class ISA(_ISA):
     """
     Basic Instruction Set Architecture specification.
 
-    :ivar str name: The QPU ISA name.
-    :ivar str version: The version of the ISA.
-    :ivar Union[int,float] timestamp: A timestamp of when the ISA was defined.
     :ivar Sequence[Qubit] qubits: The qubits associated with the ISA.
     :ivar Sequence[Edge] edges: The multi-qubit gates.
     """
@@ -50,48 +47,42 @@ class ISA(_ISA):
         The dictionary representation is of the form::
 
             {
-                "id": {
-                    "name": "example_qpu",
-                    "version": "0.1",
-                    "timestamp": "23423423"
+                "1Q": {
+                    "0": {
+                        "type": "Xhalves"
+                    },
+                    "1": {
+                        "type": "Xhalves",
+                        "dead": True
+                    },
+                    ...
                 },
-                "logical-hardware": [
-                    [
-                        {
-                            "qubit-id": 0,
-                            "type": "Xhalves",
-                            "dead": False
-                        },
-                        {
-                            "qubit-id": 1,
-                            "type": "Xhalves",
-                            "dead": False
-                        }
-                    ],
-                    [
-                        {
-                            "action-qubits": [0, 1],
-                            "type": "CZ",
-                            "dead": False
-                        }
-                    ]
-                ]
+                "2Q": {
+                    "1-4": {
+                        "type": "CZ"
+                    },
+                    "1-5": {
+                        "type": "CZ"
+                    },
+                    ...
+                },
+                ...
             }
 
         :return: A dictionary representation of self.
-        :rtype: Dict[str,Any]
+        :rtype: Dict[str, Any]
         """
 
-        def _maybe_configure(d, o, t):
+        def _maybe_configure(o, t):
             # type: (dict, Union[Qubit,Edge], str) -> dict
             """
             Exclude default values from generated dictionary.
 
-            :param dict d: The dictionary (which is also modified in place!).
             :param Union[Qubit,Edge] o: The object to serialize
             :param str t: The default value for ``o.type``.
             :return: d
             """
+            d = {}
             if o.type != t:
                 d["type"] = o.type
             if o.dead:
@@ -99,16 +90,9 @@ class ISA(_ISA):
             return d
 
         return {
-            "id": {
-                "name": self.name,
-                "version": self.version,
-                "timestamp": self.timestamp,
-            },
-            "logical-hardware": [
-                [_maybe_configure({"qubit-id": q.id}, q, DEFAULT_QUBIT_TYPE) for q in self.qubits],
-                [_maybe_configure({"action-qubits": a.targets}, a, DEFAULT_EDGE_TYPE)
-                 for a in self.edges],
-            ]
+            "1Q": {"{}".format(q.id): _maybe_configure(q, DEFAULT_QUBIT_TYPE) for q in self.qubits},
+            "2Q": {"{}-{}".format(*edge.targets): _maybe_configure(edge, DEFAULT_EDGE_TYPE)
+                   for edge in self.edges}
         }
 
     @staticmethod
@@ -121,17 +105,16 @@ class ISA(_ISA):
         :rtype: ISA
         """
         return ISA(
-            name=d["id"]["name"],
-            version=d["id"].get("version", "0.0"),
-            timestamp=d["id"].get("timestamp"),
-            qubits=[Qubit(id=q["qubit-id"],
-                          type=q.get("type", DEFAULT_QUBIT_TYPE),
-                          dead=q.get("dead", False))
-                    for q in d["logical-hardware"][0]],
-            edges=[Edge(targets=e["action-qubits"],
-                        type=e.get("type", DEFAULT_EDGE_TYPE),
-                        dead=e.get("dead", False))
-                   for e in d["logical-hardware"][1]],
+            qubits=sorted([Qubit(id=int(qid),
+                                 type=q.get("type", DEFAULT_QUBIT_TYPE),
+                                 dead=q.get("dead", False))
+                           for qid, q in d["1Q"].items()],
+                          key=lambda qubit: qubit.id),
+            edges=sorted([Edge(targets=[int(q) for q in eid.split('-')],
+                               type=e.get("type", DEFAULT_EDGE_TYPE),
+                               dead=e.get("dead", False))
+                          for eid, e in d["2Q"].items()],
+                         key=lambda edge: edge.targets),
         )
 
 

pyquil/tests/test_device.py
@@ -13,48 +13,28 @@ DEVICE_FIXTURE_NAME = 'mixed_architecture_chip'
 @pytest.fixture
 def isa_dict():
     return {
-        'id': {
-            'name': DEVICE_FIXTURE_NAME,
-            'version': '0.0',
-            'timestamp': '20180104103600'
+        "1Q": {
+            "0": {
+                'type': 'Xhalves'
+            },
+            "1": {},
+            "2": {},
+            "3": {
+                "dead": True
+            }
         },
-        'logical-hardware': [
-            [
-                {
-                    'qubit-id': 0,
-                    'type': 'Xhalves'
-                },
-                {
-                    'qubit-id': 1
-                },
-                {
-                    'qubit-id': 2
-                },
-                {
-                    'qubit-id': 3,
-                    'dead': True
-                }
-            ],
-            [
-                {
-                    'action-qubits': [0, 1],
-                    'type': 'CZ'
-                },
-                {
-                    'action-qubits': [1, 2],
-                    'type': 'ISWAP'
-                },
-                {
-                    'action-qubits': [2, 0],
-                    'type': 'CPHASE'
-                },
-                {
-                    'action-qubits': [0, 3],
-                    'type': 'CZ',
-                    'dead': True
-                }
-            ]
-        ]
+        "2Q": {
+            "0-1": {},
+            "1-2": {
+                "type": "ISWAP"
+            },
+            "2-0": {
+                "type": "CPHASE"
+            },
+            "0-3": {
+                "dead": True
+            }
+        }
     }
 
 
@@ -104,9 +84,6 @@ def device_raw(isa_dict, noise_model_dict):
 def test_isa(isa_dict):
     isa = ISA.from_dict(isa_dict)
     assert isa == ISA(
-        name=isa_dict['id']['name'],
-        version=isa_dict['id']['version'],
-        timestamp=isa_dict['id']['timestamp'],
         qubits=[
             Qubit(id=0, type='Xhalves', dead=False),
             Qubit(id=1, type='Xhalves', dead=False),
@@ -115,9 +92,9 @@ def test_isa(isa_dict):
         ],
         edges=[
             Edge(targets=[0, 1], type='CZ', dead=False),
+            Edge(targets=[0, 3], type='CZ', dead=True),
             Edge(targets=[1, 2], type='ISWAP', dead=False),
             Edge(targets=[2, 0], type='CPHASE', dead=False),
-            Edge(targets=[0, 3], type='CZ', dead=True),
         ])
     assert isa == ISA.from_dict(isa.to_dict())
 

commit 915186bee320aa2d7450d55075c5b4fd1c9c73d9

Fix various type check errors. (#338)
* Fix device.py _maybe_configure() (Broke in #312)
* Fix iffy circular imports in compiler.py and qvm.py
* Modify makefile so that 'make typecheck' excludes unit tests

Makefile
@@ -1,7 +1,7 @@
 # top-level pyquil Makefile
 
 PACKAGENAME = pyquil
-
+FILES = pyquil/*.py pyquil/api/*.py pyquil/latex/*.py pyquil/_parser/*.py
 # Kudos: Adapted from Auto-documenting default target 
 # https://marmelab.com/blog/2016/02/29/auto-documented-makefile.html
 help:
@@ -20,10 +20,10 @@ lint:		## Delint python source
 	@flake8 $(PACKAGENAME)
 
 typecheck:	## Static typechecking 
-	@mypy pyquil/*.py pyquil/*/*.py  --ignore-missing-imports --follow-imports=skip
+	@mypy $(FILES)  --ignore-missing-imports --follow-imports=skip
 
 untyped:	## Report type errors and untyped functions
-	@mypy pyquil/*.py pyquil/api/*.py pyquil/latex/*.py pyquil/_parser/*.py --ignore-missing-imports --follow-imports=skip --disallow-untyped-defs
+	@mypy $(FILES) --ignore-missing-imports --follow-imports=skip --disallow-untyped-defs
 
 docs:		## Build documentation
 	$(MAKE) -C docs html

pyquil/api/compiler.py
@@ -14,7 +14,7 @@
 #    limitations under the License.
 ##############################################################################
 
-from pyquil.api import Job
+from pyquil.api.job import Job
 from pyquil.device import Device, ISA, Specs
 from pyquil.quil import Program
 from pyquil.parser import parse_program

pyquil/api/qvm.py
@@ -17,7 +17,8 @@ import warnings
 
 from six import integer_types
 
-from pyquil.api import Job, CompilerConnection
+from pyquil.api.job import Job
+from pyquil.api.compiler import CompilerConnection
 from pyquil.quil import Program
 from pyquil.wavefunction import Wavefunction
 from pyquil.noise import apply_noise_model

pyquil/device.py
@@ -77,7 +77,7 @@ class ISA(_ISA):
         """
 
         def _maybe_configure(o, t):
-            # type: (dict, Union[Qubit,Edge], str) -> dict
+            # type: (Union[Qubit,Edge], str) -> dict
             """
             Exclude default values from generated dictionary.
 