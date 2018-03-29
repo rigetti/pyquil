fe4a072898657151fce2307250d410213bf2a23a
rht <rhtbot@protonmail.com>
2018-03-30 01:46:11 +0700

    pauli power operation: Change initial type of result to the correct one (#153)

--- pyquil/paulis.py
+++ pyquil/paulis.py
@@ -203,7 +203,7 @@ class PauliTerm(object):
         """
         if not isinstance(power, int) or power < 0:
             raise ValueError("The power must be a non-negative integer.")
-        result = 1
+        result = ID()
 
         identities = [PauliTerm('I', qubit) for qubit in self.get_qubits()]
         if not identities:
@@ -479,7 +479,7 @@ class PauliSum(object):
         """
         if not isinstance(power, int) or power < 0:
             raise ValueError("The power must be a non-negative integer.")
-        result = 1
+        result = PauliSum([ID()])
 
         if not self.get_qubits():
             # There aren't any nontrivial operators
