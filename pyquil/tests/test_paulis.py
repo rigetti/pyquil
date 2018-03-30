aec87e94090f8f7a47f75c62efe321a9d8f5beca
Mark Fingerhuth <markfingerhuth@protonmail.com>
2018-03-30 15:31:58 -0400

    fixed test_exponentiate_identity tests appropriately

--- pyquil/tests/test_paulis.py
+++ pyquil/tests/test_paulis.py
@@ -349,19 +349,19 @@ def test_exponentiate_identity():
     generator = PauliTerm("I", 1, 0.0)
     para_prog = exponential_map(generator)
     prog = para_prog(1)
-    result_prog = Program().inst([X(0), PHASE(-0.0)(0), X(0), PHASE(-0.0)(0)])
+    result_prog = Program()
     assert prog == result_prog
 
     generator = PauliTerm("I", 1, 1.0)
     para_prog = exponential_map(generator)
     prog = para_prog(1)
-    result_prog = Program().inst([X(0), PHASE(-1.0)(0), X(0), PHASE(-1.0)(0)])
+    result_prog = Program()
     assert prog == result_prog
 
     generator = PauliTerm("I", 10, 0.08)
     para_prog = exponential_map(generator)
     prog = para_prog(1)
-    result_prog = Program().inst([X(0), PHASE(-0.08)(0), X(0), PHASE(-0.08)(0)])
+    result_prog = Program()
     assert prog == result_prog
 
 
