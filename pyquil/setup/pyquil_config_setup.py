bd342d0f20d42b6967e88961686cfefbd4434f99
Matthew Harrigan <matthew.harrigan@outlook.com>
2018-06-11 12:45:57 -0700

    Robust pyquil-config-setup (#468)
    * Robust pyquil-config-setup
    
    1) try to validate the form of user id and api key and try again
       if not right
    2) make a backup of existing files
    
    * Do everything in the regex

--- pyquil/setup/pyquil_config_setup.py
+++ pyquil/setup/pyquil_config_setup.py
@@ -14,6 +14,9 @@
 #    limitations under the License.
 ##############################################################################
 
+import re
+import os
+
 from six.moves import input
 
 from pyquil.api._config import PyquilConfig
@@ -24,10 +27,46 @@ def main():
     print("Enter the required information below for Forest connections.")
     print("If you haven't signed up yet you will need to do so first at http://forest.rigetti.com")
 
-    key = input("Forest API Key: ")
-    user = input("User ID: ")
+    while True:
+        key = input("Forest API Key: ")
+        key_ma = re.match(r'^\s*(\w{40})\s*$', key)
+        if key_ma:
+            # Looks like a real key
+            key = key_ma.group(1)
+            break
+
+        print("That doesn't look like a valid API key. Try again or use Ctrl-C to quit")
+
+    while True:
+        user = input("User ID: ")
+        user_ma = re.match(r'^\s*([a-fA-F0-9]{8}-[a-fA-F0-9]{4}-[a-fA-F0-9]{4}-'
+                           r'[a-fA-F0-9]{4}-[a-fA-F0-9]{12})\s*$', user)
+        if user_ma:
+            # Looks like a real user id
+            user = user_ma.group(1)
+            break
+
+        print("That doesn't look like a valid User ID. Try again or use Ctrl-C to quit")
 
     path = PyquilConfig.DEFAULT_PYQUIL_CONFIG_PATH
+
+    if os.path.exists(path):
+        # Make a backup by appending .bak(i) where we increment
+        # i until the desired backup filename doesn't already exist
+        i = 1
+        dn = os.path.dirname(path)
+        bn = os.path.basename(path)
+        while True:
+            bak_path = os.path.join(dn, "{bn}.bak{i}".format(bn=bn, i=i))
+            if not os.path.exists(bak_path):
+                break
+            i += 1
+
+        print("I already found a file at {path}. Creating a backup at {bak_path}."
+              .format(path=path, bak_path=bak_path))
+
+        os.rename(path, bak_path)
+
     with open(path, 'w') as f:
         f.write("[" + PyquilConfig.SECTION + "]\n")
         f.write(PyquilConfig.API_KEY + ": " + key + "\n")
