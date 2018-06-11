a69893d4f100e3eb1cd5fa3bb147e53962bca895
Matthew Harrigan <matthew.harrigan@outlook.com>
2018-06-10 17:15:50 -0700

    Single source version (#461)
    per https://packaging.python.org/guides/single-sourcing-package-version/
    
    Fixes #313

--- setup.py
+++ setup.py
@@ -15,12 +15,30 @@
 #    limitations under the License.
 ##############################################################################
 
+import os
+import re
+
 from setuptools import setup, find_packages
-from pyquil import __version__
+
+HERE = os.path.abspath(os.path.dirname(__file__))
+
+
+def read(*parts):
+    with open(os.path.join(HERE, *parts), 'r') as fp:
+        return fp.read()
+
+
+def find_version(*file_paths):
+    version_file = read(*file_paths)
+    version_match = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]", version_file, re.M)
+    if version_match:
+        return version_match.group(1)
+    raise RuntimeError("Unable to find version string.")
+
 
 setup(
     name="pyquil",
-    version=__version__,
+    version=find_version('pyquil', '__init__.py'),
     author="Rigetti Computing",
     author_email="softapps@rigetti.com",
     description="A Python library to generate Quantum Instruction Language (Quil) Programs.",
