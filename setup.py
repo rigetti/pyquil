5d968444482c447fd27792ed0fbdf03c4127b6c5
Matthew Harrigan <matt.harrigan@rigetti.com>
2018-07-08 14:49:06 -0700

    Remove setup.py requirements

--- setup.py
+++ setup.py
@@ -63,20 +63,6 @@ setup(
     url="https://github.com/rigetticomputing/pyquil.git",
     packages=find_packages(exclude=["*.tests", "*.tests.*", "tests.*", "tests"]),
     license="LICENSE",
-    install_requires=[
-        'contextvars == 2.2',
-        'numpy >= 1.10',
-        'matplotlib >= 1.5',
-        'requests >= 2.4.2',
-        'typing >= 3.6',
-        'urllib3 >= 1.21.1',
-        "antlr4-python3-runtime>=4.7",
-    ],
-    setup_requires=['pytest-runner'],
-    tests_require=[
-        'pytest >= 3.0.0',
-        'mock',
-    ],
     test_suite='pyquil.tests',
     entry_points={
         'console_scripts': ['pyquil-config-setup=pyquil.setup.pyquil_config_setup:main']
