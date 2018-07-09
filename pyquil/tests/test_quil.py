7a6976fca912eaf634fe1ffef3970cf766192aa7
Tomas Babej <tomas@tbabej.com>
2018-07-09 18:39:36 -0400

    tests: Cover throwaway MEASURE instructions

--- pyquil/tests/test_quil.py
+++ pyquil/tests/test_quil.py
@@ -781,6 +781,23 @@ def test_get_classical_addresses_from_program():
     p += [MEASURE(i, i) for i in [0, 3, 1]]
     assert get_classical_addresses_from_program(p) == [0, 1, 3]
 
+def test_get_classical_addresses_from_quil_program():
+    """
+    Tests that can get_classical_addresses_from_program can handle both MEASURE
+    quil instructions with and without explicit classical registers.
+    """
+
+    p = Program('\n'.join([
+        'X 0',
+        'MEASURE 0'
+    ]))
+    assert get_classical_addresses_from_program(p) == []
+
+    p = Program('\n'.join([
+        'X 0',
+        'MEASURE 0 [1]'
+    ]))
+    assert get_classical_addresses_from_program(p) == [1]
 
 def test_pragma_with_placeholders():
     q = QubitPlaceholder()
