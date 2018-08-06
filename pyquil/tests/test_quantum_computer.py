0a4914fd8dd9e49e71eb9c470e7794e730648d82
Matthew Harrigan <matthew.harrigan@outlook.com>
2018-08-06 11:08:08 -0700

    get_qc (#517)
    * get_qc
    
    * Sensible default for connection
    
    * Flake 8
    
    * Spelling error
    
    * Lightly document noise models
    
    * Top-level import
    
    * Fix invalid use of `strip`

--- pyquil/tests/test_quantum_computer.py
+++ pyquil/tests/test_quantum_computer.py
@@ -1,13 +1,13 @@
 import networkx as nx
 import numpy as np
+import pytest
 
-from pyquil import Program
+from pyquil import Program, get_qc, list_quantum_computers
 from pyquil.api import QVM
-from pyquil.device import NxDevice, ISA, gates_in_isa
+from pyquil.api.quantum_computer import _get_flipped_protoquil_program, QuantumComputer, _parse_name
+from pyquil.device import NxDevice
 from pyquil.gates import *
-
-from pyquil.api.quantum_computer import _get_flipped_protoquil_program, QuantumComputer
-from pyquil.noise import _decoherence_noise_model, NoiseModel
+from pyquil.noise import decoherance_noise_with_asymmetric_ro
 
 
 def test_get_flipped_program():
@@ -81,25 +81,9 @@ def test_run(forest):
     assert 0 < np.mean(parity) < 0.15
 
 
-def decoherance_noise_with_asymettric_ro(isa: ISA):
-    """Reimplementation of `add_decoherance_noise` with asymmetric readout.
-
-    For simplicity, we use the default values for T1, T2, gate times, et al. and hard-code
-    readout fidelities here.
-    """
-    gates = gates_in_isa(isa)
-    noise_model = _decoherence_noise_model(gates)
-    p00 = 0.975
-    p11 = 0.911
-    aprobs = np.array([[p00, 1 - p00],
-                       [1 - p11, p11]])
-    aprobs = {q: aprobs for q in noise_model.assignment_probs.keys()}
-    return NoiseModel(noise_model.gates, aprobs)
-
-
 def test_readout_symmetrization(forest):
     device = NxDevice(nx.complete_graph(3))
-    noise_model = decoherance_noise_with_asymettric_ro(device.get_isa())
+    noise_model = decoherance_noise_with_asymmetric_ro(device.get_isa())
     qc = QuantumComputer(
         name='testy!',
         qam=QVM(connection=forest, noise_model=noise_model),
@@ -114,12 +98,102 @@ def test_readout_symmetrization(forest):
     avg0_us = np.mean(bs1[:, 0])
     avg1_us = 1 - np.mean(bs1[:, 1])
     diff_us = avg1_us - avg0_us
-    print(avg0_us, avg1_us, diff_us)
     assert diff_us > 0.03
 
     bs2 = qc.run_symmetrized_readout(prog, [0, 1], 1000)
     avg0_s = np.mean(bs2[:, 0])
     avg1_s = 1 - np.mean(bs2[:, 1])
     diff_s = avg1_s - avg0_s
-    print(avg0_s, avg1_s, diff_s)
     assert diff_s < 0.05
+
+
+def test_list_qc():
+    qc_names = list_quantum_computers()
+    assert qc_names == ['8Q-Agave', '8Q-Agave-qvm', '8Q-Agave-noisy-qvm',
+                        '19Q-Acorn', '19Q-Acorn-qvm', '19Q-Acorn-noisy-qvm',
+                        '9q-generic-qvm', '9q-generic-noisy-qvm']
+
+
+def test_parse_qc_name():
+    name, as_qvm, noisy = _parse_name('9q-generic', None, None)
+    assert name == '9q-generic'
+    assert not as_qvm
+    assert not noisy
+
+    name, as_qvm, noisy = _parse_name('9q-generic-qvm', None, None)
+    assert name == '9q-generic'
+    assert as_qvm
+    assert not noisy
+
+    name, as_qvm, noisy = _parse_name('9q-generic-noisy-qvm', None, None)
+    assert name == '9q-generic'
+    assert as_qvm
+    assert noisy
+
+
+def test_parse_qc_flags():
+    name, as_qvm, noisy = _parse_name('9q-generic', False, False)
+    assert name == '9q-generic'
+    assert not as_qvm
+    assert not noisy
+
+    name, as_qvm, noisy = _parse_name('9q-generic', True, None)
+    assert name == '9q-generic'
+    assert as_qvm
+    assert not noisy
+
+    name, as_qvm, noisy = _parse_name('9q-generic', True, True)
+    assert name == '9q-generic'
+    assert as_qvm
+    assert noisy
+
+
+def test_parse_qc_redundant():
+    name, as_qvm, noisy = _parse_name('9q-generic', False, False)
+    assert name == '9q-generic'
+    assert not as_qvm
+    assert not noisy
+
+    name, as_qvm, noisy = _parse_name('9q-generic-qvm', True, False)
+    assert name == '9q-generic'
+    assert as_qvm
+    assert not noisy
+
+    name, as_qvm, noisy = _parse_name('9q-generic-noisy-qvm', True, True)
+    assert name == '9q-generic'
+    assert as_qvm
+    assert noisy
+
+
+def test_parse_qc_conflicting():
+    with pytest.raises(ValueError) as e:
+        name, as_qvm, noisy = _parse_name('9q-generic-qvm', False, False)
+
+    assert e.match(r'.*but you have specified `as_qvm=False`')
+
+    with pytest.raises(ValueError) as e:
+        name, as_qvm, noisy = _parse_name('9q-generic-noisy-qvm', True, False)
+    assert e.match(r'.*but you have specified `noisy=False`')
+
+
+def test_parse_qc_strip():
+    # Originally used `str.strip` to remove the suffixes. This is not correct!
+    name, _, _ = _parse_name("mvq-qvm", None, None)
+    assert name == 'mvq'
+
+    name, _, _ = _parse_name("mvq-noisy-qvm", None, None)
+    assert name == 'mvq'
+
+
+def test_qc(forest):
+    qc = get_qc('9q-generic-noisy-qvm', connection=forest)
+    assert isinstance(qc, QuantumComputer)
+    assert isinstance(qc.qam, QVM)
+    assert qc.qam.noise_model is not None
+    assert qc.qubit_topology().number_of_nodes() == 9
+    assert qc.qubit_topology().degree[0] == 2
+    assert qc.qubit_topology().degree[4] == 4
+
+    # TODO: have `qubits` default to all device qubits?
+    bs = qc.run_and_measure(Program(X(0)), qubits=[0], trials=1)
+    np.testing.assert_array_equal(bs, [[1]])
