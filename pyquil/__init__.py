34ac128a3cf2bbb7e1aac8c72f549fd0a31527e3
vtomole <vtomole@iastate.edu>
2018-08-06 20:02:19 -0500

    Add CompilerConnection, Parameter, quil trig functions and DefGate to init.py

--- pyquil/__init__.py
+++ pyquil/__init__.py
@@ -1,6 +1,6 @@
 __version__ = "2.0.0.dev0"
 
-from pyquil.quil import Program
-from pyquil.api import QVMConnection, QPUConnection
-from pyquil.gates import *
-from pyquil.api import get_devices
+from pyquil.quil import Program, Pragma
+from pyquil.api import QVMConnection, QPUConnection, CompilerConnection, get_devices
+from pyquil.parameters import Parameter, quil_sin, quil_cos, quil_sqrt, quil_exp, quil_cis
+from pyquil.quilbase import DefGate
