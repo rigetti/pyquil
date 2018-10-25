5b91ff889af212ce038f101a84844ff95b043f75
Gavin Crooks <gavin@rigetti.com>
2018-10-25 14:49:24 -0700

    Add forest fixture to tests so they will skip if qvm/quilc not avaliable

--- pyquil/tests/test_examples.py
+++ pyquil/tests/test_examples.py
@@ -5,18 +5,18 @@ from subprocess import Popen, PIPE, STDOUT
 
 # As bare minimum test that all example scripts run without error
 
-def test_meyer_penny_game():
+def test_meyer_penny_game(forest):
     rval = subprocess.call(['examples/meyer_penny_game.py'])
     assert rval == 0
 
 
-def test_run_quil():
+def test_run_quil(forest):
     rval = subprocess.call(['examples/run_quil.py',
                             'examples/hello_world.quil'])
     assert rval == 0
 
 
-def test_forest2_simple_prog():
+def test_forest2_simple_prog(forest):
     rval = subprocess.call(['examples/forest2-simple-prog.py'])
     assert rval == 0
 
@@ -26,12 +26,12 @@ def test_pointer():
     assert rval == 0
 
 
-def test_qaoa_ansatz():
+def test_qaoa_ansatz(forest):
     rval = subprocess.call(['examples/qaoa_ansatz.py'])
     assert rval == 0
 
 
-def test_quantum_die():
+def test_quantum_die(forest):
     p = Popen(['examples/quantum_die.py'],
               stdout=PIPE, stdin=PIPE, stderr=STDOUT,
               universal_newlines=True)
@@ -39,16 +39,16 @@ def test_quantum_die():
     assert p.returncode == 0
 
 
-def test_teleportation():
+def test_teleportation(forest):
     rval = subprocess.call(['examples/teleportation.py'])
     assert rval == 0
 
 
-def test_website():
+def test_website(forest):
     rval = subprocess.call(['examples/website-script.py'])
     assert rval == 0
 
 
-def test_website2():
+def test_website2(forest):
     rval = subprocess.call(['examples/website-script2.py'])
     assert rval == 0
