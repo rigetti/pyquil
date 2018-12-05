f2dde96af8b855b6a6404e38b221529b9ab6682e
Matthew Harrigan <matt.harrigan@rigetti.com>
2018-12-05 10:14:27 -0800

    hidden -> not shown

--- pyquil/operator_estimation.py
+++ pyquil/operator_estimation.py
@@ -54,12 +54,17 @@ class Experiment:
 
 
 def _abbrev_program(program: Program, max_len=10):
+    """Create an abbreviated string representation of a Program.
+
+    This will join all instructions onto a single line joined by '; '. If the number of
+    instructions exceeds ``max_len``, some will be excluded from the string representation.
+    """
     program_lines = program.out().splitlines()
     if max_len is not None and len(program_lines) > max_len:
         first_n = max_len // 2
         last_n = max_len - first_n
         excluded = len(program_lines) - max_len
-        program_lines = (program_lines[:first_n] + [f'... {excluded} hidden ...']
+        program_lines = (program_lines[:first_n] + [f'... {excluded} instrs not shown ...']
                          + program_lines[-last_n:])
 
     return '; '.join(program_lines)
