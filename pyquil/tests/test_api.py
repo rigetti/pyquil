2d40095e5ca6d5e4ed096f36f51f4bd192f3db4d
Peter Karalekas <peter@rigetti.com>
2019-01-03 11:27:13 -0800

    Missed a couple of the localhost->qvm changes in test_api.py

--- pyquil/tests/test_api.py
+++ pyquil/tests/test_api.py
@@ -123,7 +123,7 @@ def test_sync_run_and_measure_mock():
         return '[[0,0],[1,1]]'
 
     with requests_mock.Mocker() as m:
-        m.post('http://127.0.0.1:5000/qvm', text=mock_response)
+        m.post('http://qvm:5000/qvm', text=mock_response)
         assert mock_qvm.run_and_measure(BELL_STATE, [0, 1], trials=2) == [[0, 0], [1, 1]]
 
     with pytest.raises(ValueError):
@@ -155,13 +155,13 @@ def test_sync_expectation_mock():
         return b'[0.0, 0.0, 1.0]'
 
     with requests_mock.Mocker() as m:
-        m.post('http://127.0.0.1:5000/qvm', content=mock_response)
+        m.post('http://qvm:5000/qvm', content=mock_response)
         result = mock_qvm.expectation(BELL_STATE, [Program(Z(0)), Program(Z(1)), Program(Z(0), Z(1))])
         exp_expected = [0.0, 0.0, 1.0]
         np.testing.assert_allclose(exp_expected, result)
 
     with requests_mock.Mocker() as m:
-        m.post('http://127.0.0.1:5000/qvm', content=mock_response)
+        m.post('http://qvm:5000/qvm', content=mock_response)
         z0 = PauliTerm("Z", 0)
         z1 = PauliTerm("Z", 1)
         z01 = z0 * z1
@@ -195,7 +195,7 @@ def test_sync_paulisum_expectation():
         return b'[1.0, 0.0, 0.0]'
 
     with requests_mock.Mocker() as m:
-        m.post('http://127.0.0.1:5000/qvm', content=mock_response)
+        m.post('http://qvm:5000/qvm', content=mock_response)
         z0 = PauliTerm("Z", 0)
         z1 = PauliTerm("Z", 1)
         z01 = z0 * z1
@@ -224,7 +224,7 @@ def test_seeded_qvm(test_device):
     with patch.object(LocalQVMCompiler, "quil_to_native_quil") as m_compile,\
             patch('pyquil.api._qvm.apply_noise_model') as m_anm,\
             requests_mock.Mocker() as m:
-        m.post('http://127.0.0.1:5000/qvm', text=mock_response)
+        m.post('http://qvm:5000/qvm', text=mock_response)
         m_compile.side_effect = [BELL_STATE]
         m_anm.side_effect = [BELL_STATE]
 
