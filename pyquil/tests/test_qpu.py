9ab66bf2b9f6867ae51951496f2bc4db2cb3bc84
Lauren Capelluto <laurencapelluto@gmail.com>
2019-01-09 13:47:04 -0800

    Gate parameter arithmetic (#707)
    * Functionally complete changes to pyquil for gate arithmetic
    
    * More useful comments
    
    * Update note about changing parsing if we switch to a better parser
    
    * Pull out replacement patch values into a func, make backwards compatible
    
    * Move expression parsing to load-time
    
    * Explain the parsing hack
    
    * Fixup bug in Function case
    
    * Improve documentation and casing
    
    * Style, more documentation
    
    * Start unit tests
    
    * Fixup issue for when we have no recalc table
    
    * Add tests
    
    * fixup style
    
    * Build patch values also needed a 'hasattr' around recalculation table
    
    * IP update to be compatible with compiler server changes
    
    * revert: session scoped -> function scoped fixture
    
    * style fixup
    
    * Make sure that the default memory spaces for parametric compilation are initialized to 0 of the correct type: must be 0.0 for REAL memory regions
    
    * Update test fixtures
    
    * Fix type of default mocked value, skip tests by default
    
    * Take timeout with QPUCompiler, default 10s timeout. Use it for qpu compiler test fixture, with 0.5s timeout
    
    * Broaden exception catching for bad protocol error when given default compiler url in test
    
    * Document new timeout parameter in QPUCompiler

--- pyquil/tests/test_qpu.py
+++ pyquil/tests/test_qpu.py
@@ -2,13 +2,17 @@ import networkx as nx
 import numpy as np
 import pytest
 
+from rpcq.messages import ParameterAref
+
+from pyquil.parser import parse
 from pyquil import Program
+from pyquil.api import QuantumComputer, QPU, QPUCompiler
 from pyquil.api._compiler import _collect_classical_memory_write_locations
 from pyquil.api._config import PyquilConfig
-from pyquil.api import QuantumComputer, QPU, QPUCompiler
 from pyquil.api._qpu import _extract_bitstrings
 from pyquil.device import NxDevice
-from pyquil.gates import X
+from pyquil.gates import I, X
+from pyquil.quilatom import Expression
 
 
 def test_qpu_run():
@@ -86,3 +90,144 @@ MEASURE 3 ro[5]
     assert np.allclose(bitstrings[:, 3], buffers["q1"][:, 1])
     assert np.allclose(bitstrings[:, 4], buffers["q2"][:, 1])
     assert np.allclose(bitstrings[:, 5], buffers["q3"])
+
+
+GATE_ARITHMETIC_PROGRAMS = [
+    Program("""
+DECLARE theta REAL[1]
+DECLARE beta REAL[1]
+DECLARE ro BIT[3]
+RX(pi/2) 0
+RZ(3*theta) 0
+RZ(beta+theta) 0
+RX(-pi/2) 0
+MEASURE 0 ro[0]
+MEASURE 1 ro[1]
+"""),
+    Program("""
+RESET
+DECLARE theta REAL[1]
+DECLARE beta REAL[1]
+DECLARE ro BIT[2]
+RX(pi/2) 0
+RZ(theta) 0
+    """),
+    Program("""
+DECLARE theta REAL[1]
+DECLARE beta REAL[1]
+DECLARE ro BIT[3]
+RX(pi/2) 0
+RZ(0.79*theta) 0
+RZ(2*beta+theta*0.5+beta+beta) 0
+RX(-pi/2) 0
+MEASURE 0 ro[0]
+MEASURE 1 ro[1]
+"""),
+    Program("""
+RX(pi) 0
+"""),
+]
+
+
+@pytest.fixture
+def mock_qpu():
+    return QPU(endpoint='tcp://not-needed:00000',
+               user="pyQuil test suite")
+
+
+@pytest.fixture
+def qpu_compiler(test_device):
+    try:
+        config = PyquilConfig()
+        compiler = QPUCompiler(endpoint=config.compiler_url, device=test_device, timeout=0.5)
+        compiler.quil_to_native_quil(Program(I(0)))
+        return compiler
+    except Exception as e:
+        return pytest.skip(f"This test requires compiler connection: {e}")
+
+
+@pytest.fixture
+def gate_arithmetic_binaries(qpu_compiler: QPUCompiler):
+    return [qpu_compiler.native_quil_to_executable(p) for p in GATE_ARITHMETIC_PROGRAMS]
+
+
+def test_load(gate_arithmetic_binaries, mock_qpu):
+    def test_binary(binary):
+        assert hasattr(binary, "recalculation_table")
+        mock_qpu.load(binary)
+        assert mock_qpu.status == 'loaded'
+        for mref, rule in mock_qpu._executable.recalculation_table.items():
+            assert isinstance(mref, ParameterAref)
+            assert isinstance(rule, Expression)
+        assert len(mock_qpu._executable.recalculation_table) in [0, 2]
+    for bin in gate_arithmetic_binaries:
+        test_binary(bin)
+
+
+def test_build_patch_tables(gate_arithmetic_binaries, mock_qpu):
+    for idx, bin in enumerate(gate_arithmetic_binaries[:-1]):
+        mock_qpu.load(bin)
+        theta = np.random.randint(-100, 100) + np.random.random()
+        beta = np.random.randint(-100, 100) + np.random.random()
+        mock_qpu.write_memory(region_name='theta', value=theta)
+        mock_qpu.write_memory(region_name='beta', value=beta)
+        patch_table = mock_qpu._build_patch_values()
+        assert 'theta' in patch_table.keys()
+        assert 'beta' in patch_table.keys()
+        if idx == 0 or idx == 2:
+            assert len(patch_table) == 3
+        for parameter_name, values in patch_table.items():
+            assert isinstance(parameter_name, str)
+            assert isinstance(values, list)
+            for v in values:
+                assert isinstance(v, float) or isinstance(v, int)
+            if (idx == 0 or idx == 2) and parameter_name not in ('theta', 'beta'):
+                assert len(values) == 2
+
+
+def test_recalculation(gate_arithmetic_binaries, mock_qpu):
+    bin = gate_arithmetic_binaries[0]
+    mock_qpu.load(bin)
+    for theta in np.linspace(0, 1, 50):
+        beta = -1 * np.random.random()
+        mock_qpu.write_memory(region_name='beta', value=beta)
+        mock_qpu.write_memory(region_name='theta', value=theta)
+        mock_qpu._update_variables_shim_with_recalculation_table()
+        assert any(np.isclose(v, 3 * theta) for v in mock_qpu._variables_shim.values())
+        assert any(np.isclose(v, theta + beta) for v in mock_qpu._variables_shim.values())
+        assert any(np.isclose(v, theta) for v in mock_qpu._variables_shim.values())
+    bin = gate_arithmetic_binaries[2]
+    mock_qpu.load(bin)
+    beta = np.random.random()
+    mock_qpu.write_memory(region_name='beta', value=beta)
+    for theta in np.linspace(0, 1, 10):
+        mock_qpu.write_memory(region_name='theta', value=theta)
+        mock_qpu._update_variables_shim_with_recalculation_table()
+        assert any(np.isclose(v, 4 * beta + 0.5 * theta) for v in mock_qpu._variables_shim.values())
+
+
+def test_resolve_mem_references(gate_arithmetic_binaries, mock_qpu):
+    def expression_test(expression, expected_val):
+        expression = parse_expression(expression)
+        assert np.isclose(mock_qpu._resolve_memory_references(expression), expected_val)
+
+    def test_theta_and_beta(theta, beta):
+        mock_qpu.write_memory(region_name='theta', value=theta)
+        mock_qpu.write_memory(region_name='beta', value=beta)
+        expression_test('sqrt(2) + theta', np.sqrt(2) + theta)
+        expression_test('beta*2 + 1', beta * 2 + 1)
+        expression_test('(beta + 2) * (1 + theta)', (beta + 2) * (1 + theta))
+        expression_test('cos(beta)*sin(theta)', np.cos(beta) * np.sin(theta))
+        expression_test('beta * theta', beta * theta)
+        expression_test('theta - beta', theta - beta)
+    # We just need the status to be loaded so we can write memory
+    mock_qpu.load(gate_arithmetic_binaries[0])
+    test_theta_and_beta(0.4, 3.1)
+    test_theta_and_beta(5, 0)
+    for _ in range(10):
+        test_theta_and_beta(np.random.random(), np.random.random() + np.random.randint(-100, 100))
+
+
+def parse_expression(expression):
+    """ We have to use this as a hack for now, RZ is meaningless. """
+    return parse(f"RZ({expression}) 0")[0].params[0]
