adf04c9b1aa074c67bb34ea06d7f18ee06bbdfd7
Matthew Harrigan <matthew.harrigan@outlook.com>
2019-01-14 19:40:24 -0800

    Fix handling of identity in operator estimation (#758)
    * Fix handling of identity in operator estimation
    
    * Add a test, actually fix the issue

--- pyquil/tests/test_operator_estimation.py
+++ pyquil/tests/test_operator_estimation.py
@@ -222,7 +222,15 @@ def test_append():
 
 def test_no_complex_coeffs(forest):
     qc = get_qc('2q-qvm')
-    suite = TomographyExperiment([ExperimentSetting(sI(), 1.j * sY(0))], program=Program(X(0), ),
+    suite = TomographyExperiment([ExperimentSetting(sI(), 1.j * sY(0))], program=Program(X(0)),
                                  qubits=[0])
     with pytest.raises(ValueError):
         res = list(measure_observables(qc, suite))
+
+
+def test_identity(forest):
+    qc = get_qc('2q-qvm')
+    suite = TomographyExperiment([ExperimentSetting(sI(), 0.123 * sI(0))],
+                                 program=Program(X(0)), qubits=[0])
+    result = list(measure_observables(qc, suite))[0]
+    assert result.expectation == 0.123
