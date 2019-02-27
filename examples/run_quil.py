347f0cf562c366b33e293770953d100fa9d3bb96
Mark Skilbeck <mark.skilbeck@rigetti.com>
2019-02-27 10:09:39 -0800

    Fix examples/run_quil.py
    It was out of date, and causing errors during the test suite.

--- examples/run_quil.py
+++ examples/run_quil.py
@@ -6,10 +6,7 @@ This module runs basic Quil text files against the Forest QVM API.
 
 from __future__ import print_function
 from argparse import ArgumentParser, ArgumentDefaultsHelpFormatter
-from pyquil.quil import Program
-from pyquil import api
-
-qvm = api.QVMConnection()
+from pyquil import Program, get_qc
 
 help_string = "Script takes two arguments. Quil program filename is required as the first " \
               "argument and number of classical registers to return is the optional second " \
@@ -19,21 +16,23 @@ help_string = "Script takes two arguments. Quil program filename is required as 
 def parse():
     parser = ArgumentParser(__doc__, formatter_class=ArgumentDefaultsHelpFormatter)
     parser.add_argument('filepath', help='Quil program filename')
-    parser.add_argument('--classical-register-num', '-n', metavar='N', default=8, type=int,
-                        help="Number of classical registers to return.")
+    parser.add_argument('--qubit-num', '-q', metavar='Q', default=8, type=int,
+                        help="Number of qubits to use.")
     args = parser.parse_args()
-    main(args.filepath, args.classical_register_num)
+    main(args.filepath, args.qubit_num)
 
 
-def main(filepath, classical_register_num):
+def main(filepath, qubit_num):
     with open(filepath) as file:
         quil_prog = file.read()
         program = Program(quil_prog)
 
+    qc = get_qc(f"{qubit_num}q-qvm")
+
     print("Running Quil Program from: ", filepath)
     print("---------------------------")
     print("Output: ")
-    print(qvm.run(program, list(range(classical_register_num))))
+    print(qc.run(program))
 
 
 if __name__ == '__main__':
