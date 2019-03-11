More useful comparison for corrected variance in 2q test

pyquil/tests/test_operator_estimation.py
@@ -1760,7 +1760,6 @@ def test_corrected_statistics_2q_nontrivial_nonentangled_state(forest):
     #       of uncorrelated errors
     qc = get_qc('2q-qvm')
     expt = ExperimentSetting(TensorProductState(), sZ(0) * sZ(1))
-    np.random.seed(0)
     theta1, theta2 = np.random.uniform(0.0, 2 * np.pi, size=2)
     p = Program(RX(theta1, 0), RX(theta2, 1))
     p00, p11, q00, q11 = np.random.uniform(0.70, 0.99, size=4)
@@ -1790,11 +1789,11 @@ def test_corrected_statistics_2q_nontrivial_nonentangled_state(forest):
     alph10 = (np.sin(theta1 / 2) * np.cos(theta2 / 2)) ** 2
     alph11 = (np.sin(theta1 / 2) * np.sin(theta2 / 2)) ** 2
     # calculate Z^{\otimes 2} expectation, and error of the mean
-    z_expectation = (alph00 + alph11) - (alph01 + alph10)
-    simulated_stddev = np.sqrt((1 - z_expectation ** 2) / num_shots)
+    expected_expectation = (alph00 + alph11) - (alph01 + alph10)
+    expected_stddev = np.sqrt(np.var(expectations))
     # compare against simulated results
-    np.testing.assert_allclose(result_expectation, z_expectation, atol=2e-2)
-    np.testing.assert_allclose(result_stddev, simulated_stddev, atol=2e-2)
+    np.testing.assert_allclose(result_expectation, expected_expectation, atol=2e-2)
+    np.testing.assert_allclose(result_stddev, expected_stddev, atol=2e-2)
 
 
 def _point_state_fidelity_estimate(v, dim=2):