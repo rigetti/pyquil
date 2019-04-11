90982cf6f760474569b968f46987009092df2322
Eric Peterson <eric@rigetti.com>
2019-04-11 14:15:19 -0400

    update tests to not expect PRAGMA ..._REWIRING

--- pyquil/tests/test_api.py
+++ pyquil/tests/test_api.py
@@ -44,7 +44,6 @@ EMPTY_PROGRAM = Program()
 BELL_STATE = Program(H(0), CNOT(0, 1))
 BELL_STATE_MEASURE = Program(H(0), CNOT(0, 1), MEASURE(0, 0), MEASURE(1, 1))
 COMPILED_BELL_STATE = Program([
-    Pragma("EXPECTED_REWIRING", ('"#(0 1 2 3)"',)),
     RZ(pi / 2, 0),
     RX(pi / 2, 0),
     RZ(-pi / 2, 1),
@@ -53,7 +52,6 @@ COMPILED_BELL_STATE = Program([
     RZ(-pi / 2, 0),
     RX(-pi / 2, 1),
     RZ(pi / 2, 1),
-    Pragma("CURRENT_REWIRING", ('"#(0 1 2 3)"',)),
     Halt()
 ])
 DUMMY_ISA_DICT = {"1Q": {"0": {}, "1": {}}, "2Q": {"0-1": {}}}
