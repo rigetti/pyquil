c55495f031390ecf335a33ebe075ac91d90cb35d
Joshua Combes <joshua@rigetti.com>
2019-06-18 08:01:45 +1000

    remove scipy dependence

--- pyquil/api/_quantum_computer.py
+++ pyquil/api/_quantum_computer.py
@@ -24,7 +24,6 @@ from contextlib import contextmanager
 
 import networkx as nx
 import numpy as np
-from scipy.linalg import hadamard
 from rpcq.messages import BinaryExecutableResponse, PyQuilExecutableResponse
 
 from pyquil.api._compiler import QPUCompiler, QVMCompiler
@@ -937,6 +936,54 @@ def _construct_orthogonal_array(num_qubits: int, strength: int = 3) -> np.ndarra
 
 def _next_power_of_2(x):
     return 1 if x == 0 else 2 ** (x - 1).bit_length()
+
+
+# The code below is directly copied from scipy see https://bit.ly/2RjAHJz, the docstrings have
+# been modified.
+def hadamard(n, dtype=int):
+    """
+    Construct a Hadamard matrix.
+    Constructs an n-by-n Hadamard matrix, using Sylvester's
+    construction.  `n` must be a power of 2.
+    Parameters
+    ----------
+    n : int
+        The order of the matrix.  `n` must be a power of 2.
+    dtype : numpy dtype
+        The data type of the array to be constructed.
+    Returns
+    -------
+    H : (n, n) ndarray
+        The Hadamard matrix.
+    Notes
+    -----
+    .. versionadded:: 0.8.0
+    Examples
+    --------
+    >>> hadamard(2, dtype=complex)
+    array([[ 1.+0.j,  1.+0.j],
+           [ 1.+0.j, -1.-0.j]])
+    >>> hadamard(4)
+    array([[ 1,  1,  1,  1],
+           [ 1, -1,  1, -1],
+           [ 1,  1, -1, -1],
+           [ 1, -1, -1,  1]])
+    """
+    if n < 1:
+        lg2 = 0
+    else:
+        lg2 = int(math.log(n, 2))
+    if 2 ** lg2 != n:
+        raise ValueError("n must be an positive integer, and n must be "
+                         "a power of 2")
+
+    H = np.array([[1]], dtype=dtype)
+
+    # Sylvester's construction
+    for i in range(0, lg2):
+        H = np.vstack((np.hstack((H, H)), np.hstack((H, -H))))
+
+    return H
 
 
 def _construct_strength_three_orthogonal_array(num_qubits: int) -> np.ndarray:
