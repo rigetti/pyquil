318734aeda1e30ff078236510eec4e3ed7e28c2a
Peter Karalekas <peter@rigetti.com>
2019-10-08 08:03:24 -0700

    Use VERSION.txt and version.py files to handle versioning

---
+++ pyquil/version.py
@@ -0,0 +1,24 @@
+##############################################################################
+# Copyright 2018 Rigetti Computing
+#
+#    Licensed under the Apache License, Version 2.0 (the "License");
+#    you may not use this file except in compliance with the License.
+#    You may obtain a copy of the License at
+#
+#        http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS,
+#    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#    See the License for the specific language governing permissions and
+#    limitations under the License.
+##############################################################################
+"""
+NOTE: This file will be overwritten by the packaging process.
+"""
+import os
+
+directory_of_this_file = os.path.dirname(os.path.abspath(__file__))
+
+with open(f'{directory_of_this_file}/../VERSION.txt', 'r') as f:
+    __version__ = f.read().strip()
