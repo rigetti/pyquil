8b72289ab8983fce069d64736abcbcb6188e0119
Mark Skilbeck <mark.skilbeck@rigetti.com>
2019-10-28 15:26:14 -0700

    Produce useful Specs in standard devices

--- pyquil/device/devices.py
+++ pyquil/device/devices.py
@@ -1,6 +1,6 @@
 import networkx as nx
 from . import Device
-from ._specs import Specs
+from ._specs import Specs, specs_from_graph
 from ._isa import ISA, isa_from_graph, isa_from_digraph
 
 
@@ -11,10 +11,10 @@ def ibmq_ourense():
         (1, 3), (3, 1),
         (3, 4), (4, 3)
     ])
-    print(isa_from_digraph(graph, twoq_type='CNOT').to_dict())
+
     return Device(name="ibmq_ourense",
                   raw={"isa": isa_from_digraph(graph, twoq_type='CNOT').to_dict(),
-                       "specs": Specs().to_dict()})
+                       "specs": specs_from_graph(graph).to_dict()})
 
 
 def ibmq_yorktown():
