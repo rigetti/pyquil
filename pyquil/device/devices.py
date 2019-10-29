f92a7c62a6a3b0e1e78e6cafe3dfce9f4603a9f1
Mark Skilbeck <mark.skilbeck@rigetti.com>
2019-10-29 12:18:25 -0700

    add google bristlecone

--- pyquil/device/devices.py
+++ pyquil/device/devices.py
@@ -28,4 +28,23 @@ def ibmq_yorktown(oneq_type='Xhalves', twoq_type='CNOT'):
                   raw={"isa": isa_from_digraph(graph, oneq_type=oneq_type, twoq_type=twoq_type).to_dict(),
                        "specs": specs_from_graph(graph).to_dict()})
 
+def google_bristlecone(oneq_type='Xhalves', twoq_type='CZ'):
+    height = 12
+    width = 6
+
+    edges = []
+    for i in range(height - 1):
+        for j in range(width):
+            qi = j + width * i
+            bi = qi + width
+            edges.append((qi, bi))
+            if i % 2 == 0 and (qi % width) > 0:
+                edges.append((qi, bi - 1))
+            elif i % 2 != 0 and ((qi + 1) % width) > 0:
+                edges.append((qi, bi + 1))
+
+    graph = nx.from_edgelist(edges)
+
+    return Device(name="google_bristlecone",
+                  raw={"isa": isa_from_graph(graph).to_dict(),
                        "specs": specs_from_graph(graph).to_dict()})
