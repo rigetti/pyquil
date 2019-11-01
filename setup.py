2758b76466b3242c2e33fb129708a0472b894004
Peter Karalekas <peter@rigetti.com>
2019-11-01 18:01:59 -0400

    Support Python 3.8 and include in the CI suite (#1093)

--- setup.py
+++ setup.py
@@ -64,7 +64,9 @@ setup(
     classifiers=[
         'Development Status :: 5 - Production/Stable',
         'License :: OSI Approved :: Apache Software License',
-        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
         'Operating System :: OS Independent',
     ],
     install_requires=[
