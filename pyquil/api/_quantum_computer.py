5780ea937b0b521f70ea1848f1a67effc9aa99b4
Tommy Moffat <tmoffat@rigetti.com>
2019-11-05 16:27:12 -0800

    Removed duplicate changes

--- pyquil/api/_quantum_computer.py
+++ pyquil/api/_quantum_computer.py
@@ -121,7 +121,9 @@ class QuantumComputer:
         self.qam.load(executable)
         if memory_map:
             for region_name, values_list in memory_map.items():
-                self.qam.write_memory(region_name=region_name, value=values_list)
+                for offset, value in enumerate(values_list):
+                    # TODO gh-658: have write_memory take a list rather than value + offset
+                    self.qam.write_memory(region_name=region_name, offset=offset, value=value)
         return self.qam.run() \
             .wait() \
             .read_memory(region_name='ro')
