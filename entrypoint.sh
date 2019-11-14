6ab989bd5911c85b76471fd8c0f0585d9fc57b26
Peter Karalekas <peter@rigetti.com>
2019-11-14 11:25:26 -0800

    Redirect stderr and don't check SDK version in entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,8 @@ echo -e "========================================="
 echo -e "=   WELCOME TO THE RIGETTI FOREST SDK   ="
 echo -e "========================================="
 echo -e "Copyright (c) 2016-2019 Rigetti Computing\n"
-/src/quilc/quilc --quiet -R &
-/src/qvm/qvm --quiet -S &
+
+/src/quilc/quilc --quiet --check-sdk-version=no -R 2> quilc.log &
+/src/qvm/qvm --quiet --check-sdk-version=no -S 2> qvm.log &
 
 exec "$@"
