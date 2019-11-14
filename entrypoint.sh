96a7aa66b85c98583a4e82088c6f40efc497f7d9
Peter Karalekas <peter@rigetti.com>
2019-11-14 13:32:04 -0800

    Restore SDK version check to the server commands

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,7 +5,7 @@ echo -e "=   WELCOME TO THE RIGETTI FOREST SDK   ="
 echo -e "========================================="
 echo -e "Copyright (c) 2016-2019 Rigetti Computing\n"
 
-/src/quilc/quilc --quiet --check-sdk-version=no -R 2> quilc.log &
-/src/qvm/qvm --quiet --check-sdk-version=no -S 2> qvm.log &
+/src/quilc/quilc --quiet -R 2> quilc.log &
+/src/qvm/qvm --quiet -S 2> qvm.log &
 
 exec "$@"
