e8985117a35c460291263f1d1865fca90e8ca635
Erik Davis <erik@rigetti.com>
2020-05-12 12:59:56 -0700

    add rewrite_arithmetic hooks for frame mutations

--- pyquil/api/tests/test_rewrite_arithmetic.py
+++ pyquil/api/tests/test_rewrite_arithmetic.py
@@ -1,4 +1,6 @@
 from pyquil.quil import Program
+from pyquil.quilatom import Qubit, Frame
+from pyquil.quilbase import DefFrame
 from pyquil.api._rewrite_arithmetic import rewrite_arithmetic
 from rpcq.messages import (
     ParameterAref,
@@ -63,4 +65,98 @@ def test_rewrite_arithmetic_mixed():
             "RZ(__P2[0]) 0",
             "RZ(__P2[1]) 0",
         ).out()
+    )
+
+
+def test_rewrite_arithmetic_set_scale():
+    prog = Program(
+        "DECLARE theta REAL",
+        'SET-SCALE 0 "rf" 1.0',
+        'SET-SCALE 0 "rf" theta',
+    )
+
+    response = rewrite_arithmetic(prog)
+
+    assert response == RewriteArithmeticResponse(
+        original_memory_descriptors={"theta": ParameterSpec(length=1, type="REAL")},
+        recalculation_table={ParameterAref(index=0, name="__P1"): "theta[0]/8"},
+        quil=Program(
+            "DECLARE __P1 REAL[1]",
+            "DECLARE theta REAL[1]",
+            'SET-SCALE 0 "rf" 1.0',
+            'SET-SCALE 0 "rf" __P1[0]',
+        ).out(),
+    )
+
+
+def test_rewrite_arithmetic_frequency():
+    fdefn0 = DefFrame(
+        frame=Frame([Qubit(0)], "rf"),
+        center_frequency=10.0,
+        sample_rate=20.0,
+    )
+    fdefn1 = DefFrame(
+        frame=Frame([Qubit(1)], "rf"),
+        sample_rate=20.0,
+    )
+    prog = Program(
+        fdefn0,
+        fdefn1, 
+        "DECLARE theta REAL",
+        'SET-FREQUENCY 0 "rf" theta',
+        'SHIFT-FREQUENCY 0 "rf" theta',
+        'SET-FREQUENCY 1 "rf" theta',
+    )
+
+    response = rewrite_arithmetic(prog)
+
+    assert response == RewriteArithmeticResponse(
+        original_memory_descriptors={"theta": ParameterSpec(length=1, type="REAL")},
+        recalculation_table={
+            ParameterAref(index=0, name="__P1"): "(theta[0] - 10.0)/20.0",
+            ParameterAref(index=1, name="__P1"): "theta[0]/20.0"
+        },
+        quil=Program(
+            fdefn0,
+            fdefn1,
+            "DECLARE __P1 REAL[2]",
+            "DECLARE theta REAL[1]",
+            'SET-FREQUENCY 0 "rf" __P1[0]',
+            'SHIFT-FREQUENCY 0 "rf" __P1[0]',
+            'SET-FREQUENCY 1 "rf" __P1[1]'
+        ).out(),
+    )
+
+
+
+def test_rewrited_arithmetic_mixed():
+    fdefn = DefFrame(
+        frame=Frame([Qubit(0)], "rf"),
+        center_frequency=10.0,
+        sample_rate=20.0,
+    )
+    prog = Program(
+        fdefn,
+        "DECLARE theta REAL",
+        'SET-FREQUENCY 0 "rf" theta',
+        'SET-PHASE 0 "rf" theta',
+        'SET-SCALE 0 "rf" theta',
+    )
+
+    response = rewrite_arithmetic(prog)
+
+    assert response == RewriteArithmeticResponse(
+        original_memory_descriptors={"theta": ParameterSpec(length=1, type="REAL")},
+        recalculation_table={
+            ParameterAref(index=0, name='__P1'): '(theta[0] - 10.0)/20.0',
+            ParameterAref(index=1, name='__P1'): 'theta[0]',
+            ParameterAref(index=2, name='__P1'): 'theta[0]/8'},
+        quil=Program(
+            fdefn,
+            "DECLARE __P1 REAL[3]",
+            "DECLARE theta REAL[1]",
+            'SET-FREQUENCY 0 "rf" __P1[0]',
+            'SET-PHASE 0 "rf" __P1[1]',
+            'SET-SCALE 0 "rf" __P1[2]',
+        ).out(),
     )
