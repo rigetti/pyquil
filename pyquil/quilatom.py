22ae4dbb8dd7606ccdf2d1b770b4158fae6c2e4f
Erik Davis <erik@rigetti.com>
2020-06-01 12:53:19 -0700

    add defcal matching and expansion

--- pyquil/quilatom.py
+++ pyquil/quilatom.py
@@ -747,6 +747,23 @@ class Addr(MemoryReference):
         super(Addr, self).__init__("ro", offset=value, declared_size=None)
 
 
+def _contained_mrefs(expression: ExpressionDesignator) -> Set[MemoryReference]:
+    """
+    Determine which memory references are contained in this expression.
+
+    :param expression: expression involving parameters
+    :return: set of parameters contained in this expression
+    """
+    if isinstance(expression, BinaryExp):
+        return _contained_mrefs(expression.op1) | _contained_mrefs(expression.op2)
+    elif isinstance(expression, Function):
+        return _contained_mrefs(expression.expression)
+    elif isinstance(expression, MemoryReference):
+        return {expression}
+    else:
+        return set()
+
+
 @dataclass(eq=True, frozen=True)
 class Frame(QuilAtom):
     """
