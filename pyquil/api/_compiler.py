10583bb1e7b5700b29e78c9c2d45cd40bbec594c
Erik Davis <erik@rigetti.com>
2020-06-22 18:50:16 -0700

    allow for compilation of quil programs with calibrations

--- pyquil/api/_compiler.py
+++ pyquil/api/_compiler.py
@@ -275,13 +275,19 @@ class QPUCompiler(AbstractCompiler):
     @_record_call
     def quil_to_native_quil(self, program: Program, *, protoquil: Optional[bool] = None) -> Program:
         self._connect_quilc()
-        request = NativeQuilRequest(quil=program.out(), target_device=self.target_device)
-        response = self.quilc_client.call(
-            "quil_to_native_quil", request, protoquil=protoquil
-        ).asdict()
-        nq_program = parse_program(response["quil"])
-        nq_program.native_quil_metadata = response["metadata"]
-        nq_program.num_shots = program.num_shots
+        try:
+            quilt_calibrations = program.calibrations
+            program.calibrations = []
+            request = NativeQuilRequest(quil=program.out(), target_device=self.target_device)
+            response = self.quilc_client.call(
+                "quil_to_native_quil", request, protoquil=protoquil
+            ).asdict()
+            nq_program = parse_program(response["quil"])
+            nq_program.native_quil_metadata = response["metadata"]
+            nq_program.num_shots = program.num_shots
+            nq_program.calibrations = quilt_calibrations
+        finally:
+            program.calibrations = quilt_calibrations
         return nq_program
 
     @_record_call
