46806d49446c5cd7426f37e9868f7ce8dac46083
Mark Skilbeck <markskilbeck@gmail.com>
2020-11-19 15:24:12 +0000

    remove unused import

--- pyquil/tests/test_parser.py
+++ pyquil/tests/test_parser.py
@@ -13,7 +13,7 @@
 #    See the License for the specific language governing permissions and
 #    limitations under the License.
 ##############################################################################
-from lark import Token, UnexpectedToken, UnexpectedCharacters
+from lark import Token, UnexpectedToken
 
 import numpy as np
 import pytest
