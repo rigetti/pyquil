2a359d119d3209fbdec5d2964f05f1b92a5b2462
Mark Hodson <mhodson@rigetti.com>
2021-09-22 21:43:46 +0930

    Initial solution working for test. Had to give away the notion of arrays for the values, and require the user to individually address the values for each memory offset.

--- test/unit/test_memory.py
+++ test/unit/test_memory.py
@@ -47,39 +47,37 @@ def test_pauli_term_to_measurement_memory_map():
 
 
 def test_substitute_memory_reference():
-    x = MemoryReference("x", 0, declared_size=2)
     x_0 = MemoryReference("x", 0, declared_size=2)
     x_1 = MemoryReference("x", 1, declared_size=2)
 
     # complete substitutions
 
-    assert substitute(x_0, {x: [+5, -5]}) == +5
-    assert substitute(x_1, {x: [+5, -5]}) == -5
+    assert substitute(x_0, {x_0: 5}) == 5
 
-    assert substitute(x_0 + x_1, {x: [+5, -5]}) == 0
-    assert substitute(x_0 - x_1, {x: [+5, -5]}) == 10
-    assert substitute(x_0 * x_1, {x: [+5, -5]}) == -25
-    assert substitute(x_0 / x_1, {x: [+5, -5]}) == -1
+    assert substitute(x_0 + x_1, {x_0: +5, x_1: -5}) == 0
+    assert substitute(x_0 - x_1, {x_0: +5, x_1: -5}) == 10
+    assert substitute(x_0 * x_1, {x_0: +5, x_1: -5}) == -25
+    assert substitute(x_0 / x_1, {x_0: +5, x_1: -5}) == -1
 
-    assert substitute(x_0 * x_0 ** 2 / x_1, {x: [5, 10]}) == 12.5
+    assert substitute(x_0 * x_0 ** 2 / x_1, {x_0: 5, x_1: 10}) == 12.5
 
-    assert np.isclose(substitute(quil_exp(x_0), {x: [5, 10]}), np.exp(5))
-    assert np.isclose(substitute(quil_sin(x_0), {x: [5, 10]}), np.sin(5))
-    assert np.isclose(substitute(quil_cos(x_0), {x: [5, 10]}), np.cos(5))
-    assert np.isclose(substitute(quil_sqrt(x), {x: [5, 10]}), np.sqrt(5))
-    assert np.isclose(substitute(quil_cis(x), {x: [5, 10]}), np.exp(1j * 5.0))
+    assert np.isclose(substitute(quil_exp(x_0), {x_0: 5, x_1: 10}), np.exp(5))
+    assert np.isclose(substitute(quil_sin(x_0), {x_0: 5, x_1: 10}), np.sin(5))
+    assert np.isclose(substitute(quil_cos(x_0), {x_0: 5, x_1: 10}), np.cos(5))
+    assert np.isclose(substitute(quil_sqrt(x_0), {x_0: 5, x_1: 10}), np.sqrt(5))
+    assert np.isclose(substitute(quil_cis(x_0), {x_0: 5, x_1: 10}), np.exp(1j * 5.0))
 
     # incomplete substitutions
 
     y = MemoryReference("y", 0, declared_size=1)
     z = MemoryReference("z", 0, declared_size=1)
 
-    assert substitute(y + z, {y: [5]}) == 5 + z
+    assert substitute(y + z, {y: 5}) == 5 + z
 
-    assert substitute(quil_cis(z), {y: [5]}) == quil_cis(z)
+    assert substitute(quil_cis(z), {y: 5}) == quil_cis(z)
 
     # array substitution pass-through
 
     a = MemoryReference("a", 0, declared_size=1)
 
-    assert np.allclose(substitute_array([quil_sin(a), quil_cos(a)], {a: [5]}), [np.sin(5), np.cos(5)])
+    assert np.allclose(substitute_array([quil_sin(a), quil_cos(a)], {a: 5}), [np.sin(5), np.cos(5)])
