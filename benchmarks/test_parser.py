fc660f7388d6de5444613071cc6d774e2763beef
Steve Manuel <nilslice@gmail.com>
2022-03-11 16:14:08 -0700

    Benchmark: include benchmark suite and automation for multiple os

---
+++ benchmarks/test_parser.py
@@ -0,0 +1,29 @@
+import pytest
+import os
+from pyquil._parser.parser import run_parser
+from pyquil.quil import Program
+
+
+def from_corpus():
+    programs = []
+    dir = os.path.join(os.path.dirname(__file__), "quilc", "tests", "good-test-files")
+    for path in os.listdir(dir):
+        filepath = os.path.join(dir, path)
+        if os.path.isfile(filepath):
+            file = open(filepath, "r")
+            program = file.read()
+            try:
+                run_parser(program)
+                programs.append((path, program))
+            except:
+                continue
+            finally:
+                file.close()
+
+    return programs
+
+
+@pytest.mark.parametrize("quil", from_corpus())
+def test_parser(benchmark, quil):
+    benchmark.group = "Parse: %s" % quil[0]
+    benchmark((lambda: Program(run_parser(quil[1]))))
