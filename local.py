f9a2e725e972aadf77f555479224c4840ada95b8
Mark Skilbeck <mark.skilbeck@rigetti.com>
2022-08-31 16:12:42 +0100

    Num shots no worky

---
+++ local.py
@@ -0,0 +1,27 @@
+import os
+import asyncio
+from unittest import result
+from pyquil.api._compiler import LocalCompiler
+from pyquil.api._qpu import QPU
+from pyquil import Program, get_qc
+
+
+QPID = os.getenv("QPID", "Aspen-11")
+
+
+async def main():
+    qc = get_qc(QPID)
+    p = Program("DECLARE ro BIT[2]", "X 0", "CNOT 0 1", "MEASURE 0 ro[0]", "MEASURE 1 ro[1]")
+    p.wrap_in_numshots_loop(10)
+    lc = LocalCompiler(quantum_processor=qc.quantum_processor, quantum_processor_id=QPID)
+    native = Program(await lc.quil_to_native_quil(p))
+    executable = await lc.native_quil_to_executable(native)
+    
+    qpu = QPU(quantum_processor_id=QPID)
+    execute_response = qpu.execute(executable=executable)
+    result_response = qpu.get_result(execute_response=execute_response)
+    print(result_response.execution_duration_microseconds, result_response.readout_data)
+
+if __name__ == "__main__":
+    asyncio.run(main())
+    
