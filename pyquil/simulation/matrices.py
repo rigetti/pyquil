711671d65cd225680ff975451b663bb8c47d413f
Bram Evert <bram.evert@gmail.com>
2023-01-26 23:46:09 +0000

    Formatting

--- pyquil/simulation/matrices.py
+++ pyquil/simulation/matrices.py
@@ -248,7 +248,7 @@ def PHASEDFSIM(theta: float, zeta: float, chi: float, gamma: float, phi: float) 
                 np.exp(-1j * (gamma - zeta)) * np.cos(theta / 2),
                 0,
             ],
-            [0, 0, 0, np.exp(1j * phi - 2j*gamma)],
+            [0, 0, 0, np.exp(1j * phi - 2j * gamma)],
         ]
     )
 
