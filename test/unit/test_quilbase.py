f7f2a41949e3373a95c4324746b2b7f7a0ac1b84
marquessv <marquessavaldez@gmail.com>
2023-02-01 15:41:45 -0800

    add test for FORKED gate

--- test/unit/test_quilbase.py
+++ test/unit/test_quilbase.py
@@ -1,5 +1,6 @@
 from math import pi
-from pyquil.quilbase import Gate
+from typing import List
+from pyquil.quilbase import Gate, MemoryReference, ParameterDesignator
 from pyquil.quilatom import Qubit
 import pytest
 
@@ -9,7 +10,10 @@ import pytest
     [
         ("X", [], [Qubit(0)]),
         ("CPHASE", [pi / 2], [Qubit(0), Qubit(1)]),
+        ("RZ", [MemoryReference("theta", 0, 1)], [Qubit(0)]),
+        ("RZ", [MemoryReference("alpha", 0) - MemoryReference("beta")], [Qubit(0)]),
     ],
+    ids=("X-Gate", "CPHASE-Expression", "RZ-MemoryReference", "RZ-MemoryReference-Expression"),
 )
 class TestGate:
     def test_str(self, name, params, qubits, snapshot):
@@ -28,8 +32,12 @@ class TestGate:
         gate = Gate(name, params, qubits)
         assert str(gate.dagger()) == snapshot
 
+    def test_forked_modifier(self, name, params, qubits, snapshot):
+        gate = Gate(name, params, qubits)
+        alt_params: List[ParameterDesignator] = [n for n in range(len(params))]
+        assert str(gate.forked(Qubit(5), alt_params)) == snapshot
+
     def test_get_qubits(self, name, params, qubits):
         gate = Gate(name, params, qubits)
-        if all(isinstance(q, Qubit) for q in qubits):
-            assert gate.get_qubits(indices=True) == {q.index for q in qubits}
+        assert gate.get_qubits(indices=True) == {q.index for q in qubits}
         assert gate.get_qubits(indices=False) == set(qubits)
