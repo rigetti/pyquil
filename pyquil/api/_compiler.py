11091c5017f5d626419a4bc54f62d2d2d46ba0d5
Marquess Valdez <marquessavaldez@gmail.com>
2023-12-07 13:16:24 -0800

    get TypeAlias from typing_extensions

--- pyquil/api/_compiler.py
+++ pyquil/api/_compiler.py
@@ -13,7 +13,8 @@
 #    See the License for the specific language governing permissions and
 #    limitations under the License.
 ##############################################################################
-from typing import Any, Dict, Optional, TypeAlias
+from typing import Any, Dict, Optional
+from typing_extensions import TypeAlias
 from warnings import warn
 
 from qcs_sdk import QCSClient
