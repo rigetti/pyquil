c00e67d82bfdef1931eb10ba687573e6fbeb3e7f
Jake Selig <jselig@rigetti.com>
2024-04-05 15:49:18 -0600

    chore: overwrite __package__ reference with grpc_web name when publishing alt grpc_web package

--- scripts/ci_publish_grpc_web.py
+++ scripts/ci_publish_grpc_web.py
@@ -17,10 +17,19 @@ with open(join(workspace_path, "pyproject.toml"), "r+") as f:
     data = toml.load(f)
 
     data["project"] = { "name": "pyquil-grpc-web" }
-    # data["tool"]["poetry"]["name"] = "pyquil-grpc-web"
+    data["tool"]["poetry"]["name"] = "pyquil-grpc-web"
 
     deps = data["tool"]["poetry"]["dependencies"]
     deps["qcs-sdk-python-grpc-web"] = deps["qcs-sdk-python"]
     del deps["qcs-sdk-python"]
 
     write(f, data)
+
+# The `__package__` will refer to `pyquil`, but this
+# package is now `pyquil_grpc_web` - this is the simplest
+# way to make that overwrite.
+with open(join(workspace_path, "pyquil", "_version.py"), "r+") as f:
+    updated = f.read().replace("__package__", '"pyquil_grpc_web"')
+    f.seek(0)
+    f.truncate()
+    f.write(updated)
