3dc2f57e17d815db1afadbb6f57960b2bf387dd6
Marquess Valdez <marquessavaldez@gmail.com>
2024-04-15 18:05:39 -0700

    formatting

--- pyquil/quil.py
+++ pyquil/quil.py
@@ -1041,6 +1041,7 @@ class Program:
         """
         return _convert_to_py_instructions(self._program.to_instructions())
 
+
 def merge_with_pauli_noise(
     prog_list: Iterable[Program], probabilities: Sequence[float], qubits: Sequence[int]
 ) -> Program:
