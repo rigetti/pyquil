75696ecfd0d416598437d9746f6a122e9164d848
Marquess Valdez <marquessavaldez@gmail.com>
2024-05-20 09:18:05 -0700

    move import

--- pyquil/__init__.py
+++ pyquil/__init__.py
@@ -1,10 +1,10 @@
 import os
 
-from pyquil._core import *
 from pyquil._version import pyquil_version
 from pyquil.api import get_qc, list_quantum_computers
 
 if os.environ.get("PYQUIL_EXPERIMENTAL_BACKEND", False):
+    from pyquil._core import *
     from pyquil._core.program import Program
 else:
     from pyquil.quil import Program
