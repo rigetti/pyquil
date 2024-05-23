19ab61f53e7e182e356acb421d0aea4456afeb3f
Marquess Valdez <marquessavaldez@gmail.com>
2024-05-23 15:28:51 -0700

    remove nest_asyncio

--- test/e2e/test_e2e.py
+++ test/e2e/test_e2e.py
@@ -15,17 +15,13 @@
 ##############################################################################
 from multiprocessing.pool import ThreadPool
 
-import nest_asyncio
 import numpy as np
 
 from pyquil import Program
-from pyquil.api import QuantumComputer, QPU
-from pyquil.gates import H, CNOT, MEASURE, RX
+from pyquil.api import QPU, QuantumComputer
+from pyquil.gates import CNOT, MEASURE, RX, H
 from pyquil.quilatom import MemoryReference
 from pyquil.quilbase import Declare
-
-nest_asyncio.apply()
-
 
 TEST_PROGRAM = Program(
     Declare("ro", "BIT", 2),
